9ae072d3436fda5f1c9ff7f262b10b0344c18c97
EnzoDevSoft <enzodevsoft@gmail.com>
2025-05-27 21:11:55 -0300

    Corrigido export default no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+// src/App.jsx
 import React from 'react';
 
 export default function App() {
-  return ;
+  return (
+    <div>
+      <h1>Olá, sou Enzo Gattelli 👋</h1>
+      <p>Este é meu portfólio com React e Vite!</p>
+    </div>
+  );
 }
