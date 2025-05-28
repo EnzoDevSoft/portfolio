4fb61364bde8195f90f7eb40739b8bc830606c5d
EnzoDevSoft <enzodevsoft@gmail.com>
2025-05-27 21:18:04 -0300

    Adicionado export default App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import React from 'react';
 export default function App() {
   return (
     <div>
-      <h1>Olá, sou Enzo Gattelli 👋</h1>
-      <p>Este é meu portfólio com React e Vite!</p>
+      
     </div>
   );
 }
